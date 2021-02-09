159150ae77cca73674ae9d9a28876e6f191c02cf
chenfengshf <chenfengshf@hotmail.com>
2021-02-09 17:16:57 +0800

    1st next app

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,8 +12,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <>
+          <Link href='posts/first-post'><a>Read this page for first </a></Link>
+        </>
 
         <p className={styles.description}>
           Get started by editing{' '}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default () =>{
+    return (
+        <>
+            <h1> <Link href="/">---BACK TO HOME </Link></h1>
+            <p>this is the first page</p>
+        </>
+    )
+}
